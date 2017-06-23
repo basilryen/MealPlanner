44e92a8d131b239121878e2bd97f7f1c0c9c4acf
basilryen <basilryen@gmail.com>
2017-06-22 20:46:40 -0700

    Adds card view to My Recipes, Meal planner and Add Ingredient

--- app/src/main/java/com/teambazomi/mealplanner/RecipeListAdapter.java
+++ app/src/main/java/com/teambazomi/mealplanner/RecipeListAdapter.java
@@ -1,6 +1,7 @@
 package com.teambazomi.mealplanner;
 
 import android.support.v7.widget.RecyclerView;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
